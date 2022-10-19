dd99ebb2564eefcf0fde0e0e908cd5ac9fc43bf2
AGOETERS <113448483+AGOETERS@users.noreply.github.com>
2022-10-19 08:45:26 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (randomPick == 1) {
+        info.changeScoreBy(1)
+    }
+    generate()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (randomPick == 0) {
+        info.changeScoreBy(1)
+    }
+    generate()
+})
+function generate () {
+    randomPick = randint(0, 1)
+    if (randomPick == 0) {
+        mySprite.say("push A")
+    }
+    if (randomPick == 1) {
+        mySprite.say("push B")
+    }
+}
+let randomPick = 0
+let mySprite: Sprite = null
+game.splash("Push A or B as directed")
+mySprite = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 d 4 c . . 
+    . . . . b 5 5 1 f f d d 4 4 4 b 
+    . . . . b 5 5 d f b 4 4 4 4 b . 
+    . . . b d 5 5 5 5 4 4 4 4 b . . 
+    . . b d d 5 5 5 5 5 5 5 5 b . . 
+    . b d d d d 5 5 5 5 5 5 5 5 b . 
+    b d d d b b b 5 5 5 5 5 5 5 b . 
+    c d d b 5 5 d c 5 5 5 5 5 5 b . 
+    c b b d 5 d c d 5 5 5 5 5 5 b . 
+    . b 5 5 b c d d 5 5 5 5 5 d b . 
+    b b c c c d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+info.startCountdown(20)
+generate()
